e9a18390e5822be5dc041c1fb73f7c81ecb8fd57
Simi Sur <gaurabbose40@gmail.com>
2023-06-09 22:52:43 +0600

    admin class api initialized

--- index.js
+++ index.js
@@ -187,12 +187,22 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/classes/:email", async (req, res) => {
-      const email = req.params.email;
-      const query = { email: email };
-      const result = await classes.find(query).toArray();
-      res.send(result);
+    app.get("/classes", verifyJWToken, verifyAdmin, async (req, res) => {
+      const allClasses = await classes.find().toArray();
+      res.send(allClasses);
     });
+
+    app.get(
+      "/classes/:email",
+      verifyJWToken,
+      verifyInstructor,
+      async (req, res) => {
+        const email = req.params.email;
+        const query = { email: email };
+        const result = await classes.find(query).toArray();
+        res.send(result);
+      }
+    );
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
